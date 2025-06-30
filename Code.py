d739f77573f5a20631af1d9c87d73f147b62705f
thriveni632 <thrivenit40@gmail.com>
2025-06-30 16:43:58 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,11 @@
+traffic_volume_estimation/
+├── data/                # Raw and processed data
+├── notebooks/           # Jupyter notebooks
+├── src/
+│   ├── preprocessing.py
+│   ├── models/
+│   └── utils.py
+├── experiments/         # Model performance and experiments
+├── results/             # Plots, metrics, predictions
+├── requirements.txt
+└── README.md
